fbc63253c677d34d680541ff037a35a90d3fef35
Vincent Datrier <vincent@datrier.com>
2023-03-07 17:27:30 +0100

    Enhance logic on getting geoposts

--- web/app/plugins/nortia-cgplocator/src/Geolocator/GeolocatorComponent.php
+++ web/app/plugins/nortia-cgplocator/src/Geolocator/GeolocatorComponent.php
@@ -20,7 +20,7 @@ class GeolocatorComponent
     public function get_posts_by_latlng($args = [
         'lat' => 0.0,
         'lng' => 0.0,
-        'distance' => 0.0,
+        'distance' => 0,
         'numberposts' => -1,
         'offset' => 0,
     ])
@@ -29,6 +29,18 @@ class GeolocatorComponent
         global $wpdb;
 
         $earth_radius = 3959;
+
+        if ($args['distance'] > 0) {
+            $distance = "HAVING distance < '" . esc_sql( $args['distance'] ) . "'";
+        } else {
+            $distance = "";
+        }
+
+        if ($args['numberposts'] > 0) {
+            $limit = "LIMIT " . $args['offset']. ", " . $args['numberposts'];
+        } else {
+            $limit = "";
+        }
 
         $sql = $wpdb->prepare(
             "SELECT DISTINCT
@@ -49,29 +61,24 @@ class GeolocatorComponent
             AND p.post_status = 'publish'
             AND latitude.meta_key = '_latitude'
             AND longitude.meta_key = '_longitude'
-            HAVING distance < %s
-            ORDER BY distance ASC",
+            $distance
+            ORDER BY distance ASC
+            $limit",
             $earth_radius,
             $args['lat'],
             $args['lng'],
             $args['lat'],
-            $args['distance'],
         );
 
-        $nearbyLocations = $wpdb->get_results($sql);
+        
 
+        $nearbyLocations = $wpdb->get_results($sql);
         $results = false;
 
         if (!empty($nearbyLocations)) {
             $results = array();
 
-            if($args['numberposts'] > 0) {
-                $postsToFetch = array_slice($nearbyLocations, $args['offset'], $args['numberposts']);
-            } else {
-                $postsToFetch = $nearbyLocations;
-            }
-
-            foreach ($postsToFetch as $key => $post) {
+            foreach ($nearbyLocations as $key => $post) {
                 $results[] = get_post($post->ID);
                 // Add distance to post object
                 $results[$key]->distance = $post->distance;
