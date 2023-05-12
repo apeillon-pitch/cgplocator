7b9b58b353b2427f7d339b321650f7710ece0cfc
Amaury Peillon <apeillon@digital-village.fr>
2023-05-12 17:49:04 +0200

    Update modal-disclaimer.blade.php

--- web/app/themes/pitch-theme-sage-10/resources/views/partials/template-parts/modal-disclaimer.blade.php
+++ web/app/themes/pitch-theme-sage-10/resources/views/partials/template-parts/modal-disclaimer.blade.php
@@ -16,36 +16,9 @@
             </div>
             <div class="modal-body">
                 <div class="container">
-                    <div class="row justify-content-center">
-                        @if ($options_data['modal_login']['group_repeater'])
-                            @foreach($options_data['modal_login']['group_repeater'] as $group)
-                                @if($group['section_title'])
-                                    <div class="col-12 text-center">
-                                        <span class="section-title">{!! $group['section_title'] !!}</span>
-                                    </div>
-                                @endif
-                                @foreach($group['link_repeater'] as $block)
-                                    <div class="col-12 col-lg-6 mb-4">
-                                        <div class="wrapper d-flex flex-column h-100 justify-content-between">
-                                            @if ($block['title'])
-                                                <h3 class="block-title">{!! $block['title'] !!}</h3>
-                                            @endif
-                                            @if ($block['text'])
-                                                <p>{!! $block['text'] !!}</p>
-                                            @endif
-                                            @if ($block['link_url'] && $block['link_title'])
-                                                <div>
-                                                    <a href="{{ $block['link_url'] }}" class="btn btn-primary arrow" target="_blank">
-                                                        {!! $block['link_title'] !!}
-                                                    </a>
-                                                </div>
-                                            @endif
-                                        </div>
-                                    </div>
-                                @endforeach
-                            @endforeach
-                        @endif
-                    </div>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse id tincidunt leo, non pharetra ligula. Nunc volutpat dolor tempus lectus consectetur, tempor rutrum tellus scelerisque. Vestibulum dolor odio, rutrum at commodo sed, commodo vel eros. Aenean et lectus id orci eleifend tincidunt nec vitae ligula. Vivamus non justo iaculis, ultricies leo eu, ullamcorper ipsum. Aliquam pulvinar et ante nec iaculis. Aenean turpis turpis, auctor eget massa a, iaculis volutpat elit. Nam et varius orci. Suspendisse sed accumsan magna. Nam vel ex id nulla accumsan tempor. In in neque placerat, lobortis ligula vel, tincidunt erat. Aenean eget gravida dolor.</p>
+
+                  <p>Pellentesque eget mollis libero, sed finibus lorem. Sed felis augue, sodales in posuere et, fermentum ac sapien. Morbi a lorem quis quam tristique hendrerit. Maecenas fringilla non arcu et sollicitudin. Nullam libero nibh, lacinia molestie gravida quis, mattis in arcu. Vestibulum velit tellus, ultricies sed venenatis vel, volutpat vel est. Praesent cursus mi eu velit dapibus, eget volutpat arcu porta. Nullam pellentesque volutpat velit, vitae facilisis dolor tristique vitae.</p>
                 </div>
             </div>
         </div>
