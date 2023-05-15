7bc39755788268866087cb76c463ae7cbd758ad0
Amaury Peillon <apeillon@digital-village.fr>
2023-05-15 09:54:04 +0200

    modal disclaimer

--- web/app/themes/pitch-theme-sage-10/resources/views/partials/template-parts/modal-disclaimer.blade.php
+++ web/app/themes/pitch-theme-sage-10/resources/views/partials/template-parts/modal-disclaimer.blade.php
@@ -1,26 +1,33 @@
 <div class="modal fade" id="disclaimerModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <div class="d-flex flex-row w-100 justify-content-between align-items-center">
-                    <div class="d-flex">
-                        <a href="{{ site_url() }}">
-                            <img src="{!! $header_data['data']['logo']['url'] !!}" class="img-fluid logo"
-                                 alt="{!! $header_data['data']['logo']['alt'] !!}">
-                        </a>
-                    </div>
-                    <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
-                        <i class="fa-light fa-xmark"></i>
-                    </button>
-                </div>
-            </div>
-            <div class="modal-body">
-                <div class="container">
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse id tincidunt leo, non pharetra ligula. Nunc volutpat dolor tempus lectus consectetur, tempor rutrum tellus scelerisque. Vestibulum dolor odio, rutrum at commodo sed, commodo vel eros. Aenean et lectus id orci eleifend tincidunt nec vitae ligula. Vivamus non justo iaculis, ultricies leo eu, ullamcorper ipsum. Aliquam pulvinar et ante nec iaculis. Aenean turpis turpis, auctor eget massa a, iaculis volutpat elit. Nam et varius orci. Suspendisse sed accumsan magna. Nam vel ex id nulla accumsan tempor. In in neque placerat, lobortis ligula vel, tincidunt erat. Aenean eget gravida dolor.</p>
-
-                  <p>Pellentesque eget mollis libero, sed finibus lorem. Sed felis augue, sodales in posuere et, fermentum ac sapien. Morbi a lorem quis quam tristique hendrerit. Maecenas fringilla non arcu et sollicitudin. Nullam libero nibh, lacinia molestie gravida quis, mattis in arcu. Vestibulum velit tellus, ultricies sed venenatis vel, volutpat vel est. Praesent cursus mi eu velit dapibus, eget volutpat arcu porta. Nullam pellentesque volutpat velit, vitae facilisis dolor tristique vitae.</p>
-                </div>
-            </div>
+  <div class="modal-dialog modal-dialog-centered modal-lg">
+    <div class="modal-content">
+      <div class="modal-header">
+        <div class="d-flex flex-row w-100 justify-content-between align-items-center">
+          <div class="d-flex">
+            <span class="section-title">Informations</span>
+          </div>
+          <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+            <i class="fa-light fa-xmark"></i>
+          </button>
         </div>
+      </div>
+      <div class="modal-body">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse id tincidunt leo, non pharetra ligula.
+          Nunc volutpat dolor tempus lectus consectetur, tempor rutrum tellus scelerisque. Vestibulum dolor odio, rutrum
+          at commodo sed, commodo vel eros. Aenean et lectus id orci eleifend tincidunt nec vitae ligula. Vivamus non
+          justo iaculis, ultricies leo eu, ullamcorper ipsum. Aliquam pulvinar et ante nec iaculis. Aenean turpis
+          turpis, auctor eget massa a, iaculis volutpat elit. Nam et varius orci. Suspendisse sed accumsan magna. Nam
+          vel ex id nulla accumsan tempor. In in neque placerat, lobortis ligula vel, tincidunt erat. Aenean eget
+          gravida dolor.</p>
+
+        <p>Pellentesque eget mollis libero, sed finibus lorem. Sed felis augue, sodales in posuere et, fermentum ac
+          sapien. Morbi a lorem quis quam tristique hendrerit. Maecenas fringilla non arcu et sollicitudin. Nullam
+          libero nibh, lacinia molestie gravida quis, mattis in arcu. Vestibulum velit tellus, ultricies sed venenatis
+          vel, volutpat vel est. Praesent cursus mi eu velit dapibus, eget volutpat arcu porta. Nullam pellentesque
+          volutpat velit, vitae facilisis dolor tristique vitae.</p>
+
+        <button class="btn btn-primary" data-bs-dismiss="modal" aria-label="Close">J'ai compris</button>
+      </div>
     </div>
+  </div>
 </div>
